c0b6842dad1457ba70d515799a297cc98ea12d3a
Ray <ray@Rays-MacBook-Air.local>
2025-04-30 20:04:03 +0900

    sum_of_squares_of_even refinished

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     The sum of the squares of all even numbers in the list.
     """
     sum = 0
-    for i in even_int_list[i]:
+    for i in even_int_list:
         sum = sum + i**2
         
     return sum
@@ -39,7 +39,7 @@ def main():
     int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     even_int_list = even_list(int_list)
     output = sum_of_squares_of_even(even_int_list)
-    print(even_int_list)
+    print(output)
  
 # Boilerplate code
 if __name__ == "__main__":
